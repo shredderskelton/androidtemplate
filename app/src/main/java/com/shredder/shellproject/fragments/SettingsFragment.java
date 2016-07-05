ad8e64a8a020f11cfcb5479a3ffc8c95c2437185
Nick Skelton <nick.skelton@sixt.com>
2016-07-05 16:23:59 +0200

    versioning

--- app/src/main/java/com/shredder/shellproject/fragments/SettingsFragment.java
+++ app/src/main/java/com/shredder/shellproject/fragments/SettingsFragment.java
@@ -1,22 +1,38 @@
 package com.shredder.shellproject.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.shredder.shellproject.BuildConfig;
 import com.shredder.shellproject.R;
 import com.shredder.shellproject.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SettingsFragment extends BaseFragment {
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
     }
 
+    @Bind(R.id.version_text)
+    TextView versionText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        versionText.setText(BuildConfig.VERSION_NAME);
     }
 
     @Override
