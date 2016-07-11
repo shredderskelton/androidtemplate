e1cbb2713996c14ffc3f3a108087c8e821d5661a
Nick Skelton <nick.skelton@sixt.com>
2016-07-11 10:01:08 +0200

    test duplicate

---
+++ app/src/main/java/com/shredder/shellproject/base/AddFragmentHandlerDuplicate.java
@@ -0,0 +1,47 @@
+package com.shredder.shellproject.base;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
+import com.shredder.shellproject.R;
+
+// The methods in this class are shared by both the BaseActivity and the BaseFragment.
+// Really they belong in both classes, but I refactored them out here to prevent code duplication
+public class AddFragmentHandlerDuplicate {
+    private final FragmentManager fragmentManager;
+
+    public AddFragmentHandlerDuplicate(FragmentManager fragmentManager) {
+        this.fragmentManager = fragmentManager;
+    }
+
+    public void add(BaseFragment fragment) {
+        //don't add a fragment of the same type on top of itself.
+        BaseFragment currentFragment = getCurrentFragment();
+        if (currentFragment != null) {
+            if (currentFragment.getClass() == fragment.getClass()) {
+                Log.w("Fragment Manager", "Tried to add a fragment of the same type to the backstack. This may be done on purpose in some circumstances but generally should be avoided.");
+                return;
+            }
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_content, fragment, fragment.getTitle());
+        fragmentTransaction.addToBackStack(fragment.getTitle());
+        fragmentTransaction.commit();
+    }
+
+    @Nullable
+    public BaseFragment getCurrentFragment() {
+        if (fragmentManager.getBackStackEntryCount() == 0) {
+            return null;
+        }
+        FragmentManager.BackStackEntry currentEntry = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1);
+
+        String tag = currentEntry.getName();
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        return (BaseFragment) fragment;
+    }
+}
