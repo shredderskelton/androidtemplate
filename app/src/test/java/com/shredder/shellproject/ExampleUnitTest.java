f05f735f11fade588a686bcbf590eb2eb23ea759
Nick Skelton <nick.skelton@sixt.com>
2016-07-13 19:49:30 +0200

    Revert "failed test"
    This reverts commit 4cc518f6690abf7099f035ed7e9e40e20a5b31be.

--- app/src/test/java/com/shredder/shellproject/ExampleUnitTest.java
+++ app/src/test/java/com/shredder/shellproject/ExampleUnitTest.java
@@ -28,7 +28,7 @@ public class ExampleUnitTest {
     @Test
     public void book_comparator_equal_isCorrect() throws Exception {
         Book a = new Book("IRONMAN");
-        Book z = new Book("IRONAN");
+        Book z = new Book("IRONMAN");
         int result = new BookComparator().compare(a, z);
         assertTrue(result == 0);
     }
