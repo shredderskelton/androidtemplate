b47cd79f1703726732551a72ceab93a342b26a71
Nick Skelton <nick.skelton@sixt.com>
2016-07-14 15:13:13 +0200

    hockey app settings

---
+++ app/src/alpha/java/com/shredder/shellproject/ShredderBuildSettings.java
@@ -0,0 +1,6 @@
+package com.shredder.shellproject;
+
+
+public class ShredderBuildSettings extends ShredderBuildSettingsBase {
+    public static final String HockeyAppId = "c46d126c113a466da56d51e2eb6523d6";
+}
---
+++ app/src/beta/java/com/shredder/shellproject/ShredderBuildSettings.java
@@ -0,0 +1,6 @@
+package com.shredder.shellproject;
+
+
+public class ShredderBuildSettings extends ShredderBuildSettingsBase{
+    public static final String HockeyAppId = "f6833b071e4240929927a7580933755f";
+}
